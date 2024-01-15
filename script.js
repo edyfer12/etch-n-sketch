cbe93dcb7f676bab0785b6043601276a61aced26
Edin <edyfer2@live.com.au>
2024-01-15 13:28:01 +0100

    Add code

--- script.js
+++ script.js
@@ -3,7 +3,13 @@
     For example, 50 X 50 is shown.
 */
 //Capture the reference for the parent element so child element can get appended
+const rangeBox = document.querySelector('.group-range');
 //Create an element node using JavaScript to be able to display values
+const currentGrid = document.createElement('p');
 //When user clicks the button on the range slider, 
+input[type="range"].addEventListener('click', () => {
     //append the element to the parent element that holds the range slider, minimum and maximum values
+    rangeBox.appendChild(currentGrid);
     //display the text content on the created element as current value of row multiplied by current value of column
+    currentGrid.textContent = input[type="range"].value;
+});
