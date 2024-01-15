5407c6086847f16f87d3b223df942ec6e99d123b
Edin <edyfer2@live.com.au>
2024-01-15 13:20:57 +0100

    Add comments

--- script.js
+++ script.js
@@ -2,4 +2,8 @@
     When the user clicks on the thumb of the range slider, display the value of the grid positioned at the bottom of the thumb.
     For example, 50 X 50 is shown.
 */
-
+//Capture the reference for the parent element so child element can get appended
+//Create an element node using JavaScript to be able to display values
+//When user clicks the button on the range slider, 
+    //append the element to the parent element that holds the range slider, minimum and maximum values
+    //display the text content on the created element as current value of row multiplied by current value of column
