e8ee4021ea59fb2aaab9829a73791cac6b5bde0c
Edin <edyfer2@live.com.au>
2024-01-15 16:52:34 +0100

    Add comments

--- script.js
+++ script.js
@@ -18,19 +18,53 @@ rangeSlider.addEventListener('input', () => {
 */
 //*********************************(1,1) DEFAULT ROW,COLUMN***********************************************************/
 //Capture the reference based on the sketchpad container containing rows and columns of grids
-const sketchpad = document.querySelector('#sketchpad');
+/*const sketchpad = document.querySelector('#sketchpad');
 //Create an element based on one row and one column of the grid inside the sketchpad
 const grid = document.createElement('div');
 //Append the element displaying grid of square as the child element to the parent element, the sketchpad
 sketchpad.appendChild(grid);
 //When user clicks on the grid inside the sketchpad, change the background colour from white to black
-grid.addEventListener('click',() => grid.style.background = 'black');
+grid.addEventListener('click',() => grid.style.background = 'black');*/
 
 /***************** ALGORITHM FOR SKETCHING THE PIXELATED IMAGE OR COLOURING ON THE SKETCHPAD *************************/
 
 //1. As a default, have the row as 1 and column as 1 as the default grid value on the range slider is 1 X 1
-//2. Have the number of grids on each row and column displayed evenly on the sketchpad based on the value of range slider
+    //Refer to the value of the range slider
+//2. Ensure the number of grids on each row and column displayed evenly on the sketchpad based on the value of range slider
+    //Set the initial counter to 0 where it indicates the start of the row 
+    //Loop from row 0 to a value of rows in the range slider value
+        //Create the row element 
+        //Display the row element 
+        //Set the flex property of row element as 1 that is evenly scaled within the sketchpad
+        //Set the initial counter to 0 where it indicates the start of the column for each row
+        //Loop from column 0 to the value of the range slider
 //3. Display the borders in black colour of 0.5 pixels for the grids
-//4. If the user clicks and holds on the grid that is not clicked and moves around, change the background color to black 
+            //Create grid element
+            //Display grid element
+            //Set the flex property of grid element to 1 that is even scaled within the row element
+            //Increment counter by 1 to create next grid element
+    //Increment counter by 1 to create next row element
+//4. If the user clicks and holds on the grid that is not clicked and hovers, change the background color to black 
+    //Exit loop
+    //Use event listener on the grid and set event to mousedown
+        //Set the background colour of grid element to black
 //5. If the user changes the value on the range slider, reset the sketchpad replacing all black grids back to white grids
+    //Use event listener on the range slider and set event to input
+        //Set counter variable to number of rows
+        //Loop from number of rows to 0
+            //Delete the row element as grids are deleted automatically
+            //Decrement the counter by 1 deleting next row element
 //6. Display the rows and columns based on chosen number of grids on the sketchpad
+        //Exit loop
+        //Set the initial counter to 0 where it indicates the start of the row 
+        //Loop from row 0 to a value of rows in the range slider value
+            //Create the row element 
+            //Display the row element 
+            //Set the flex property of row element as 1 that is evenly scaled within the sketchpad
+            //Set the initial counter to 0 where it indicates the start of the column for each row
+            //Loop from column 0 to the value of the range slider
+                //Create grid element
+                //Display grid element
+                //Set the flex property of grid element to 1 that is even scaled within the row element
+                //Increment counter by 1 to create next grid element
+            //Increment counter by 1 to create next row element
