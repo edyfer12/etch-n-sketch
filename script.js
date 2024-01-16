775094243fa5ef04433b9ad0c83488456827a5a3
Edin <edyfer2@live.com.au>
2024-01-16 21:19:01 +0100

    Edit and add HTML code and comments

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ const rangeSlider = document.querySelector('input[type="range"]');
 const gridInput = document.querySelector('#gridValue');
 
 /***************** ALGORITHM FOR SKETCHING THE PIXELATED IMAGE OR COLOURING ON THE SKETCHPAD *************************/
-
+/********This is where the range slider has been used************************** */
 //1. As a default, have the row as 1 and column as 1 as the default grid value on the range slider is 1 X 1
 //Refer to the value of the range slider
 let gridValue = rangeSlider.value;
@@ -50,7 +50,7 @@ for(;rowCounter < gridValue;){
 //Exit loop after all the rows and columns are created so user can use the mouse and clicks and hover on the grids to change 
 //background colour to black from white
 //Capture the reference for the grid
-let gridReference = document.querySelectorAll('.grid');
+gridReference = document.querySelectorAll('.grid');
 //Create an array that can store all the grids
 let grids = Array.from(gridReference);
 //Set the mouse down toggle to false
@@ -65,59 +65,46 @@ grids.forEach(grid => {
     //If the user clicks on the grid, set background colour to black and set the mouse down toggle to true
     grid.addEventListener('mousedown', () => {grid.style.background = 'black'; mousedown = true});
 });
+
 //Add an array so each array item can be iterated to each row
 let rows = Array.from(document.querySelectorAll('.row'));
-//Store the original value from the range slider
-//let originalRangeSliderValue = rangeSlider.value;
+
+/********************This is where the range slider is used***************************************************** */
 //5. If the user changes the value on the range slider,
-rangeSlider.addEventListener('input', () => {//display the text content on the created element as current value of row multiplied by current value of column
+rangeSlider.addEventListener('input', () => {
+    //display the text content on the created element as current value of row multiplied by current value of column
     gridInput.textContent = rangeSlider.value + 'X' + rangeSlider.value;
-    //Remove all the rows and columns
-    //Loop through each item in the array that store rows
-    rows.forEach(row => {
-        //Loop through each item in the array that store grids
-        grids.forEach(grid => {
-            //Remove the grid per row on the sketchpad
-            grid.remove();
-        });
-        //Remove the row once all the grids are removed and move to the next row
-        row.remove();
-    });
-
-    //Empty both arrays
-    rows = [];
-    grids = [];
-
-    //Create new rows and columns based on the range slider value
-    //Set the row counter to 0
+    //Remove all the rows that will automatically remove the grid elements
     let rowCounter = 0;
-    //Loop from 0 to the current range value for row
-    for(;rowCounter < rangeSlider.value;){
-        //Create row element
-        let row = document.createElement('div');
-        //Set class of row element to row
-        row.classList = 'row';
-        row.style.flex = '1';
-        //Display the row element that was already created
-        sketchpad.appendChild(row);
-        //Set grid counter to 0
-        let gridCounter = 0;
-        //Loop from 0 to the current range value for the grid
-        for(;gridCounter< rangeSlider.value;){
-            //Create grid element
-            let grid = document.createElement('div');
-            //Set class of grid element to grid
-            grid.classList = 'grid';
-            grid.style.flex = '1';
-            //Display the grid element that was already created
-            row.appendChild(grid);
-            //Increment grid counter by 1
-            gridCounter++;
+    rows.forEach((rowItem) => {sketchpad.removeChild(rowItem)});
+    //Create and display row elements
+    for(let i = 0; i < rangeSlider.value; i++){
+        let rowItem = document.createElement('div');
+        rowItem.classList = 'row';
+        rowItem.style.flex = '1';
+        sketchpad.appendChild(rowItem);
+        //Create and display grid elements
+        for(let j = 0; j < rangeSlider.value; j++){
+            let gridItem = document.createElement('div');
+            gridItem.classList = 'grid';
+            gridItem.style.flex = '1';
+            rowItem.appendChild(gridItem);
         }
-        //Increment row counter by 1
-        rowCounter++;
     }
-    //Exit loop
-    //rows = Array.from(document.querySelectorAll('row'));
-    //grids = Array.from(document.querySelectorAll('grid'));
+    //Add arrays so next time the user plays with the range slider all the div elements are stored
+    rows = Array.from(document.querySelectorAll('.row'));
+    grids = Array.from(document.querySelectorAll('.grid'));
+    //Set the mouse down toggle to false
+    let mousedown = false;
+    //If the mouse buttons are released on any elements on the page,
+    window.addEventListener('mouseup', () => mousedown = false);
+    //Loop through the each individual grid within the array that store in all the grids
+    grids.forEach(grid => {
+        //4. If the user clicks and holds on the grid that is white and hovers, change the background color to black 
+        //If the user hovers over the grids and the mouse down toggle is true, set background colour to black
+        grid.addEventListener('mouseover', () => {if(mousedown)grid.style.background = 'black'})
+        //If the user clicks on the grid, set background colour to black and set the mouse down toggle to true
+        grid.addEventListener('mousedown', () => {grid.style.background = 'black'; mousedown = true});
+});
+
 });
